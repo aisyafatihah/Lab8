3ffbdf38ff94cc2706408748dc0adfccc48628c2
Aisya Fatihah <aisyafatihahazhar@gmail.com>
2021-03-18 20:56:00 +0800

    Commit Message

--- samples/src/demo/GitTest.java
+++ samples/src/demo/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Git is fun");
+		System.out.print("Git is cool");
 	}
 
 }
